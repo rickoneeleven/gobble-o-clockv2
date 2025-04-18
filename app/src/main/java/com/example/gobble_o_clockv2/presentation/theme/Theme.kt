bcba2f50c3a640812aaa2a209bdffca5cb431758
Ryan Alexander Partington <pinescore@outlook.com>
2025-04-18 14:52:11 +0100

    ``` feat: Initial project setup for Wear OS app
    This commit introduces the foundational structure and files for a new Wear OS application.
    
    Key changes include:
    
    -   **Theme:** Added a basic `Gobbleoclockv2Theme` in `Theme.kt` for initial theming.
    -   **Gradle Wrapper:** Introduced `gradlew` and `gradlew.bat` for Gradle execution.
    -   **Splash Screen:** Defined a splash screen theme with `MainActivityTheme.Starting` in `styles.xml`.
    -   **Gradle Dependencies:** Added a `libs.versions.toml` file to manage dependencies and versions. This includes dependencies for Wear Compose, core splashscreen, and activity compose.
    - **String Resources**: Added default string resources `strings.xml` and rounded string resources `strings.xml`
    -   **Main Activity:** Implemented a basic `MainActivity` with a splash screen, and a `WearApp` composable function displaying a greeting and the current time.
    - **Lint**: Added linting configuration.
    - **App Icon**: Added icons for the application.
    - **Gradle and Build Files**: Created `build.gradle.kts` for module level and root level, `settings.gradle.kts` for settings, and `gradle.properties` for project wide settings.
    - **App Manifest:** Added `AndroidManifest.xml` with the necessary metadata for a standalone Wear OS app.
    - **Splash Icon**: Added a splash icon image.
    - **Git Ignore**: Added git ignore files.
    - **Proguard Rules**: Added default proguard rules.
    
    This setup provides a functional base for further development.
    ```

---
+++ app/src/main/java/com/example/gobble_o_clockv2/presentation/theme/Theme.kt
@@ -0,0 +1,17 @@
+package com.example.gobble_o_clockv2.presentation.theme
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material.MaterialTheme
+
+@Composable
+fun Gobbleoclockv2Theme(
+    content: @Composable () -> Unit
+) {
+    /**
+     * Empty theme to customize for your app.
+     * See: https://developer.android.com/jetpack/compose/designsystems/custom
+     */
+    MaterialTheme(
+        content = content
+    )
+}
